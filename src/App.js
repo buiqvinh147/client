02cc4d4cd442dba9b60d2d95652ffed4c6cbf5c7
BuiVinh <buiqvinh147@gmail.com>
2021-12-25 23:40:59 +0700

    oke

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="travail-app">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-colums">
+        <header>Brainstrom</header>
+        <ul>
+          <li></li>  
+        </ul> 
+        <footer>Add another card</footer>
+      </div>
+  
+     
     </div>
   );
 }

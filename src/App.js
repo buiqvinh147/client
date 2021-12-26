33162b332207e5eaa8724ad3d59561c0684e93cf
BuiVinh <buiqvinh147@gmail.com>
2021-12-26 18:23:52 +0700

    second

--- src/App.js
+++ src/App.js
@@ -5,12 +5,137 @@ function App() {
     <div className="travail-app">
       <nav className="navbar app">App Bar</nav>
       <nav className="navbar board">Board Bar</nav>
-      <div className="board-colums">
+      <div className="board-columns">
+        <div className="column">
         <header>Brainstrom</header>
         <ul>
-          <li></li>  
+          <li>
+            {/* <img src="https://quangduc.com/images/file/aT7nHPOU0AgBAB8b/cat-07.jpg" alt="traild-alt-img"></img> */}
+            Title: uiapp
+          </li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li>  
+          
         </ul> 
         <footer>Add another card</footer>
+        </div>
+        <div className="column">
+        <header>Brainstrom</header>
+        <ul>
+          <li>
+            <img src="https://quangduc.com/images/file/aT7nHPOU0AgBAB8b/cat-07.jpg" alt="traild-alt-img"></img>
+            Title: uiapp
+          </li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li> 
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li> 
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul> 
+        <footer>Add another card</footer>
+        </div>
+        <div className="column">
+        <header>Brainstrom</header>
+        <ul>
+          <li>
+            <img src="https://quangduc.com/images/file/aT7nHPOU0AgBAB8b/cat-07.jpg" alt="traild-alt-img"></img>
+            Title: uiapp
+          </li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li> 
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li> 
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul> 
+        <footer>Add another card</footer>
+        </div>
+        <div className="column">
+        <header>Brainstrom</header>
+        <ul>
+          <li>
+            <img src="https://quangduc.com/images/file/aT7nHPOU0AgBAB8b/cat-07.jpg" alt="traild-alt-img"></img>
+            Title: uiapp
+          </li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li> 
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li> 
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul> 
+        <footer>Add another card</footer>
+        </div>
+        <div className="column">
+        <header>Brainstrom</header>
+        <ul>
+          <li>
+            <img src="https://quangduc.com/images/file/aT7nHPOU0AgBAB8b/cat-07.jpg" alt="traild-alt-img"></img>
+            Title: uiapp
+          </li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li> 
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li> 
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul> 
+        <footer>Add another card</footer>
+        </div>
+        <div className="column">
+        <header>Brainstrom</header>
+        <ul>
+          <li>
+            <img src="https://quangduc.com/images/file/aT7nHPOU0AgBAB8b/cat-07.jpg" alt="traild-alt-img"></img>
+            Title: uiapp
+          </li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li> 
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li> 
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul> 
+        <footer>Add another card</footer>
+        </div>
+        <div className="column">
+        <header>Brainstrom</header>
+        <ul>
+          <li>
+            <img src="https://quangduc.com/images/file/aT7nHPOU0AgBAB8b/cat-07.jpg" alt="traild-alt-img"></img>
+            Title: uiapp
+          </li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li>  
+          <li>Add what you'd like to work on below</li> 
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li> 
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+          <li>Add what you'd like to work on below</li>
+        </ul> 
+        <footer>Add another card</footer>
+        </div>
+        
       </div>
   
      
@@ -19,3 +144,4 @@ function App() {
 }
 
 export default App;
+

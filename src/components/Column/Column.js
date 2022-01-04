f8959e8ad389966f472f52dcf15100d2ffc0ae1e
BuiVinh <buiqvinh147@gmail.com>
2022-01-04 23:58:24 +0700

    next9

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,90 @@
-import React from "react"
+import React, { useState, useEffect, useCallback } from "react"
 import { Container, Draggable } from 'react-smooth-dnd';
+import { Dropdown, Form } from 'react-bootstrap';
 import './Column.scss'
 import Card from "../Card/Card"
+import ConfirmModal from "../Common/ConfirmModal";
 import { mapOrder } from '../../utilities/sorts'
+import { MODAL_ACTIONS_CONFIRM } from '../../utilities/constants'
 
 function Column (props) {
-    const { column, onCardDrop } = props
+    const { column, onCardDrop, onUpdateColumn } = props
     const cards = mapOrder(column.cards, column.cardOrder, 'id')
-   
+    const [showConfirmModal, setShowConfirmModal] = useState(false)
+    const toggleShowConfirmModal = () => setShowConfirmModal (!showConfirmModal)
+    const [columnTitle, setColumnTitle] = useState('')
+    const handleColumnTitleChange = useCallback((e) => setColumnTitle(e.target.value), [])
+
+    useEffect(() => {
+        setColumnTitle(column.title)
+    }, [column.title])
+
+    const onConfirmModalAction = (type) => { 
+        if (type === MODAL_ACTIONS_CONFIRM) {
+            const newColumn = {
+                ...column,
+                _destroy: true
+            
+            }
+            onUpdateColumn(newColumn)
+        }
+        
+        toggleShowConfirmModal() 
+    } 
+    const selectAllInlineText = (e) => {
+        e.target.focus()
+        e.target.select()
+    }
+
+    const handleColumnTitleBlur = () => {
+        const newColumn = {
+            ...column,
+           title: columnTitle
+        
+        }
+        onUpdateColumn(newColumn)
+    }
+    const saveContentAfterPressEnter = (e) => {
+        if (e.key ==='Enter') {
+            e.preventDefault()
+            e.target.blur()
+        }
+    }
     return (
         <div className="column">
-        <header className="column-drag-handle">{column.title}</header>
+        <header className="column-drag-handle">
+            <div className="column-title">
+                
+                <Form.Control
+                    size="sm"
+                    type="text"
+                    className='travail-content-editable' 
+                    value={columnTitle}
+                    spellCheck="false"
+                    onClick={selectAllInlineText}
+                    onChange={handleColumnTitleChange}
+                    onBlur={handleColumnTitleBlur}
+                    onKeyDown={saveContentAfterPressEnter}
+                    onMouseDown={e => e.preventDefault()}
+                    // onKeyDown={event => (event.key === 'Enter') && addNewColumn()}
+                />
+            </div>
+            <div className="column-dropdown-actions">
+            <Dropdown>
+                 <Dropdown.Toggle  id="dropdown-basic" size="sm" className="dropdown-btn" />
+                     
+                
+
+                <Dropdown.Menu>
+                 <Dropdown.Item>Add Card...</Dropdown.Item>
+                 <Dropdown.Item onClick={toggleShowConfirmModal}>Remove Column...</Dropdown.Item>
+                 <Dropdown.Item>More all card in this column...</Dropdown.Item>
+                 <Dropdown.Item>Archive all card in this column...</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+            </div>
+            
+        </header>
         <div className="card-list">
         <Container
                     groupName="travail-columns"
@@ -40,6 +114,13 @@ function Column (props) {
             </div>
             
         </footer>
+
+        <ConfirmModal 
+            show={showConfirmModal}
+            onAction={onConfirmModalAction}
+            title="Remove column"
+            content={`Are you sure you want to remove <strong>${column.title}</strong>. <br/> All related cards will also be removed!`}
+        />
         </div>
     )
 }
